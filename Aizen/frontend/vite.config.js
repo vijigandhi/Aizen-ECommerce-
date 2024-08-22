b5ecfbee9bce9d19e6ffe19bf84f8782a804d243
Aki <akilashwarranpdckap@gmail.com>
2024-08-22 16:40:56 +0530

    Optimize chunk sizes and adjust Vite config for deployment

--- Aizen/frontend/vite.config.js
+++ Aizen/frontend/vite.config.js
@@ -1,15 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Aizen/',
   plugins: [react()],
   resolve: {
     alias: {
       'react-dom/client': 'react-dom/client',
     },
   },
-  server:{
-    port:3000,
-  }
-})
+  server: {
+    port: 3000,
+  },
+  build: {
+    // Increase the chunk size warning limit to suppress the warning or make it less sensitive
+    chunkSizeWarningLimit: 1000, // You can adjust this value as needed
+
+    // Configure Rollup options to manually chunk your dependencies
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Extract react and react-dom into separate chunks
+            if (id.includes('react')) {
+              return 'react';
+            }
+            // Create a chunk for all other node_modules
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
