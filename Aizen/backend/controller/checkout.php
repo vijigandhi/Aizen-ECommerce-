f234d71d8505d80cb676dcb50479e388c81e9e6b
“vigneshcdckap” <vigneshcdckap@gmail.com>
2024-08-28 12:00:54 +0530

    checkout email img

--- Aizen/backend/controller/checkout.php
+++ Aizen/backend/controller/checkout.php
@@ -218,7 +218,7 @@ class CheckoutController
             }
     
             // Content
-            $imageUrl = 'https://cdn.discordapp.com/attachments/1163859351568650270/1277507116361580544/Successful_purchase-pana.png?ex=66cd6ab7&is=66cc1937&hm=2e24fd0b45aa0c880b0add9e5db01b890ac63424b1543a5dc4c41646293d27b5&';
+            $imageUrl = 'https://cdn.discordapp.com/attachments/1163859351568650270/1277507116361580544/Successful_purchase-pana.png?ex=66cf64f7&is=66ce1377&hm=46f21165a33fd221285dcb8b51df6b2cdc15074e2de562cd22176b71169a1c62&';
     
             $mail->isHTML(true);
             $mail->Subject = 'Order Confirmation';
